0f90a7067f11ae04d4f09149d521c29e61c96814
kylthehero <kylthehero@gmail.com>
2017-06-27 17:17:32 +0000

    initial commit

---
+++ app/controllers/portfolios_controller.rb
@@ -0,0 +1,73 @@
+class PortfoliosController < ApplicationController
+  
+  def index
+    @portfolio_items = Portfolio.all
+  end
+  
+  def angular
+    @angular_portfolio_items = Portfolio.angular
+  end
+  
+  def new
+    @portfolio_item = Portfolio.new
+    3.times { @portfolio_item.technologies.build }
+  end
+  
+  def create
+    @portfolio_item = Portfolio.new(portfolio_params)
+
+    respond_to do |format|
+      if @portfolio_item.save
+        format.html { redirect_to portfolios_path, notice: 'Your portfolio item is now live.' }
+        # format.json { render :show, status: :created, location: @blog }
+      else
+        format.html { render :new }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def edit
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def update
+    @portfolio_item = Portfolio.find(params[:id])
+    respond_to do |format|
+      if @portfolio_item.update(portfolio_params)
+        format.html { redirect_to portfolios_path, notice: 'The record successfully updated.' }
+        # format.json { render :show, status: :ok, location: @blog }
+      else
+        format.html { render :edit }
+        # format.json { render json: @blog.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+  
+  def show
+    @portfolio_item = Portfolio.find(params[:id])
+  end
+  
+  def destroy
+    #look up
+    @portfolio_item = Portfolio.find(params[:id])
+    # destroy it
+    @portfolio_item.destroy
+    #redirect
+    respond_to do |format|
+      format.html { redirect_to portfolios_url, notice: 'Record was removed.' }
+      #format.json { head :no_content }
+    end
+  end
+  
+  private
+  
+  def portfolio_params
+    params.require(:portfolio).permit(:title, 
+                                      :subtitle, 
+                                      :body, 
+                                      technologies_attributes: [:name]
+                                      )
+  end
+  
+end
