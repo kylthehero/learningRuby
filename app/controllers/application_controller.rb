87bd5bc0528b1f85070b87dc83b7ddeb0169c7bd
kylthehero <kylthehero@gmail.com>
2017-07-05 17:34:39 +0000

    added petergate gem

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,19 @@ class ApplicationController < ActionController::Base
   include SetSource
   include CurrentUserConcern
   include SetTitle
+  
+  before_action :set_copyright
+  
+  def set_copyright
+    @copyright = DevcampViewTool:: Renderer.copyright 'Kyle Christensen', 'All rights reserved'
+  end
+end
+
+
+module DevcampViewTool
+   class Renderer
+     def self.copyright name, msg
+       "&copy; #{Time.now.year} | <b>#{name}</b>"
+     end
+   end
 end
